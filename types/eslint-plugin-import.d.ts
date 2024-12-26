811e4094edb8b8dd56b849b031e38c03ba0dcc04
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-12-26 11:33:28 -0800

    refactor(types): update eslint-plugin-import dummy definition file

--- types/eslint-plugin-import.d.ts
+++ types/eslint-plugin-import.d.ts
@@ -2,6 +2,5 @@ declare module 'eslint-plugin-import' {
   // {@symbiote/notExtraneous @types/eslint}
   import type { Linter } from 'eslint';
 
-  const eslintPlugin: { flatConfigs: Record<string, Linter.Config> };
-  export default eslintPlugin;
+  export const flatConfigs: Record<string, Linter.Config>;
 }
