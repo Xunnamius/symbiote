bcf89f8be89231bd3dc24d6c3fb5b58d55f3e2db
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-12-28 00:18:55 -0800

    chore: move pseudodecorator into transformer

--- src/assets/transformers/_turbo.json.ts
+++ src/assets/transformers/_turbo.json.ts
@@ -3,6 +3,8 @@ import { turboConfigProjectBase } from 'multiverse+project-utils:fs.ts';
 import { generateRootOnlyAssets, makeTransformer } from 'universe:assets.ts';
 import { stringifyJson } from 'universe:util.ts';
 
+// {@symbiote/notExtraneous turbo}
+
 export const { transformer } = makeTransformer(function (context) {
   const { toProjectAbsolutePath } = context;
 
