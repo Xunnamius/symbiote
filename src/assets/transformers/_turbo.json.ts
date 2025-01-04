6210727d4bc9b20c2064df6f0a987bc509ba512a
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-01-03 17:44:49 -0800

    fix(assets/templates): disable turbo config generation for now and add stashed configs

--- src/assets/transformers/_turbo.json.ts
+++ src/assets/transformers/_turbo.json.ts
@@ -1,26 +1,104 @@
-import {
-  turboConfigProjectBase,
-  type RelativePath
-} from 'multiverse+project-utils:fs.ts';
+// TODO: this whole transformer
+// import { isRootPackage, ProjectAttribute } from 'multiverse+project-utils:analyze.ts';
+
+// import {
+//   turboConfigProjectBase,
+//   type RelativePath
+// } from 'multiverse+project-utils:fs.ts';
 
 import {
-  compileTemplate,
-  generateRootOnlyAssets,
+  // AssetPreset,
+  // compileTemplate,
+  // definedNonBasicAssetPresets,
+  // generatePerPackageAssets,
   makeTransformer
+  // type TransformerContext
 } from 'universe:assets.ts';
 
 // {@symbiote/notExtraneous turbo}
 
-export const { transformer } = makeTransformer(function (context) {
-  const { toProjectAbsolutePath } = context;
+//const turboInitTaskName = '//#turbo:init';
 
-  // * Only the root package gets these files
-  return generateRootOnlyAssets(context, async function () {
-    return [
-      {
-        path: toProjectAbsolutePath(turboConfigProjectBase),
-        generate: () => compileTemplate(turboConfigProjectBase as RelativePath, context)
-      }
-    ];
-  });
+export const { transformer } = makeTransformer(async function (/* context */) {
+  return [];
+  // const { assetPreset } = context;
+
+  // // * Do not generate any files when using the "wrong" preset
+  // if (
+  //   definedNonBasicAssetPresets.includes(assetPreset) &&
+  //   assetPreset !== AssetPreset.TurboOnly
+  // ) {
+  //   return [];
+  // }
+
+  // // * Every package gets these files including non-hybrid monorepo roots
+  // return generatePerPackageAssets(
+  //   context,
+  //   async function ({ toPackageAbsolutePath, contextWithCwdPackage }) {
+  //     const path = toPackageAbsolutePath(turboConfigProjectBase);
+
+  //     return [
+  //       {
+  //         path,
+  //         generate: async () => {
+  //           return replaceStandardStrings(
+  //             await compileTemplate(
+  //               turboConfigProjectBase as RelativePath,
+  //               contextWithCwdPackage
+  //             ),
+  //             contextWithCwdPackage
+  //           );
+  //         }
+  //       }
+  //     ];
+  //   },
+  //   { includeRootPackageInNonHybridMonorepo: true }
+  // );
 });
+
+// async function replaceStandardStrings(
+//   content: string,
+//   {
+//     toProjectAbsolutePath,
+//     repoName,
+//     assetPreset,
+//     projectMetadata: {
+//       type,
+//       cwdPackage,
+//       rootPackage: { attributes: projectAttributes }
+//     },
+//     toPackageAbsolutePath
+//   }: TransformerContext
+// ) {
+//   // TODO: in content, replace `turboInitTaskName` but also `"{{` and the `}}"`
+//   // TODO: (turboInitTaskName should be deleted in hybridrepo and polyrepo)
+
+//   const isNonHybridMonorepoRoot =
+//     isRootPackage(cwdPackage) &&
+//     projectAttributes[ProjectAttribute.Monorepo] &&
+//     !projectAttributes[ProjectAttribute.Hybridrepo];
+
+//   return content
+//     .replaceAll(
+//       `"{{turboInitTaskName}}"`,
+//       isNonHybridMonorepoRoot ? JSON.stringify(turboInitTaskName) : ''
+//     )
+//     .replaceAll(
+//       `"{{globalEnv}}"`,
+//       isNonHybridMonorepoRoot ? JSON.stringify(globalEnv).slice(1, -1) : ''
+//     )
+//     .replaceAll(
+//       `"{{globalPassThroughEnv}}"`,
+//       isNonHybridMonorepoRoot ? JSON.stringify(globalPassThroughEnv).slice(1, -1) : ''
+//     )
+//     .replaceAll(
+//       `"{{globalDependencies}}"`,
+//       isNonHybridMonorepoRoot ? JSON.stringify(globalDependencies).slice(1, -1) : ''
+//     )
+//     .replaceAll(`//"extends": ["//"],`, '"extends": ["//"],');
+// }
+
+// const globalEnv: string[] = ['DEBUG_*', 'SSH_*', 'SYMBIOTE_', 'NODE_ENV', 'APP_ENV'];
+// const globalPassThroughEnv: string[] = [];
+// const globalDependencies: string[] = [];
+// const buildInputsExcludes: string[] = ['!test/**'];
