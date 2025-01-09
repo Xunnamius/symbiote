af354d0d777efcad54c5b9fef571837497afd230
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-01-09 09:18:47 -0800

    fix(assets/transformers): do not erroneously report jest-extended as an invalid dep

--- src/assets/transformers/_test.ts
+++ src/assets/transformers/_test.ts
@@ -31,6 +31,9 @@ export * from '@-xun/test';`
  **  bootstrapping the runtime for every test file.
  */
 
+// ? jest-extended will always come from @-xun/symbiote (i.e. transitively)
+// {@symbiote/notInvalid jest-extended}
+
 // ? https://github.com/jest-community/jest-extended#typescript
 import 'jest-extended';
 import 'jest-extended/all';`
