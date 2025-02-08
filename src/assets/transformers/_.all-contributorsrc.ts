3d179662eb95d4846d6a633df915db21d917e993
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-02-08 10:56:06 -0800

    fix(assets/transformers): only recreate all-contributors file if it does not already exist

--- src/assets/transformers/_.all-contributorsrc.ts
+++ src/assets/transformers/_.all-contributorsrc.ts
@@ -1,4 +1,4 @@
-import { allContributorsConfigProjectBase } from '@-xun/project';
+import { allContributorsConfigProjectBase, isAccessible } from '@-xun/project';
 
 import {
   generateRootOnlyAssets,
@@ -12,6 +12,7 @@ import { stringifyJson } from 'universe:util.ts';
 
 export const { transformer } = makeTransformer(function (context) {
   const { toProjectAbsolutePath, assetPreset, repoName } = context;
+  const path = toProjectAbsolutePath(allContributorsConfigProjectBase);
 
   // * Do not generate any files when using the "wrong" preset
   if (!libAssetPresets.includes(assetPreset)) {
@@ -20,32 +21,42 @@ export const { transformer } = makeTransformer(function (context) {
 
   // * Only the root package gets these files
   return generateRootOnlyAssets(context, async function () {
-    return [
-      {
-        path: toProjectAbsolutePath(allContributorsConfigProjectBase),
-        generate: () =>
-          stringifyJson({
-            projectName: repoName,
-            projectOwner: 'Xunnamius',
-            repoType: 'github',
-            repoHost: 'https://github.com',
-            files: ['README.md'],
-            imageSize: 100,
-            commit: false,
-            commitConvention: 'angular',
-            contributors: [
-              {
-                login: 'Xunnamius',
-                name: 'Bernard',
-                avatar_url: 'https://avatars.githubusercontent.com/u/656017?v=4',
-                profile: 'https://xunn.io/',
-                contributions: ['infra', 'code', 'doc', 'maintenance', 'test', 'review']
-              }
-            ],
-            contributorsPerLine: 7,
-            linkToUsage: true
-          })
-      }
-    ];
+    // ? Only create this file if it doesn't already exist
+    if (!(await isAccessible(path, { useCached: true }))) {
+      return [
+        {
+          path,
+          generate: () =>
+            stringifyJson({
+              projectName: repoName,
+              projectOwner: 'Xunnamius',
+              repoType: 'github',
+              repoHost: 'https://github.com',
+              files: ['README.md'],
+              imageSize: 100,
+              commit: false,
+              commitConvention: 'angular',
+              contributors: [
+                {
+                  login: 'Xunnamius',
+                  name: 'Bernard',
+                  avatar_url: 'https://avatars.githubusercontent.com/u/656017?v=4',
+                  profile: 'https://xunn.io/',
+                  contributions: [
+                    'infra',
+                    'code',
+                    'doc',
+                    'maintenance',
+                    'test',
+                    'review'
+                  ]
+                }
+              ],
+              contributorsPerLine: 7,
+              linkToUsage: true
+            })
+        }
+      ];
+    }
   });
 });
