2bc8b381e8a1496f68e05f7360436ba962392df8
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-03-07 21:27:40 -0800

    Revert "fix(assets/transformers): re-enable n/no-unsupported-features/node-builtins eslint check"
    This reverts commit 594125e0a0a658798b48545998a0d8652a7ce699.

--- src/assets/transformers/_eslint.config.mjs.ts
+++ src/assets/transformers/_eslint.config.mjs.ts
@@ -432,7 +432,7 @@ function nodeRules(
     // * Node (eslint-plugin-n) rules (affects all JS/TS files)
     'n/hashbang': 'warn',
     'n/no-unpublished-bin': 'error',
-    // ? Handled by eslint-plugin-import
+    // ? Handled (albeit badly currently) by eslint-plugin-import
     'n/file-extension-in-import': 'off',
     // ? Handled by symbiote project lint and symbiote build distributables
     'n/no-missing-import': 'off',
@@ -450,7 +450,8 @@ function nodeRules(
     'n/no-unsupported-features/es-builtins': 'off',
     // ? Handled by babel and core-js
     'n/no-unsupported-features/es-syntax': 'off',
-    'n/no-unsupported-features/node-builtins': 'error',
+    // ? Handled by babel and core-js
+    'n/no-unsupported-features/node-builtins': 'off',
     // ? Handled by unicorn
     'n/no-process-exit': 'off',
     'n/no-restricted-import': ['warn', restrictedImportRules],
