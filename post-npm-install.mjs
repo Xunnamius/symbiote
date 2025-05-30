b4ce62825fc0ab0648e371a38e522f8ee71b6ea1
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-05-30 03:02:06 -0700

    chore(post-npm-install): update commentary

--- post-npm-install.mjs
+++ post-npm-install.mjs
@@ -2,3 +2,5 @@
 // ! It is imperative that operations performed by this script are IDEMPOTENT!
 
 import '@-xun/common-dummies/post-install';
+
+// TODO: should also probably run "npm run build" too
