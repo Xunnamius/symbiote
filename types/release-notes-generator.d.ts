450f56aebb4b9ee6be666259169f3898916253ca
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-06-02 15:40:12 -0700

    fix(types): update types for tstyche@4

--- types/release-notes-generator.d.ts
+++ types/release-notes-generator.d.ts
@@ -2,7 +2,7 @@
 declare module '@semantic-release/release-notes-generator' {
   import type { GenerateNotesContext } from 'semantic-release';
 
-  export async function generateNotes(
+  export function generateNotes(
     pluginConfig: { parserOpts: any; writerOpts: any },
     context: GenerateNotesContext
   ): Promise<string>;
