a10abb915626bbd2a3cfb7aa2dc4e75c85e438c4
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-08-31 17:10:39 -0700

    fix(assets/transformers): allow `transformSelectEsmPackagesToCjs` to accept regular expressions in call sig

--- src/assets/transformers/_jest.config.mjs.ts
+++ src/assets/transformers/_jest.config.mjs.ts
@@ -304,7 +304,7 @@ export function assertEnvironment(): Omit<
  */
 export function transformSelectEsmPackagesToCjs(
   config: JestConfig,
-  packageNames: string[]
+  packageNames: (string | RegExp)[]
 ): void {
   const transformIgnorePatterns = [
     `/node_modules/(?!(${packageNames
