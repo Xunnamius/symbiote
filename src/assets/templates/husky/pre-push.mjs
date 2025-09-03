79d395cced979d17188580f3f3b776aa6e57df18
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2025-09-03 15:58:26 -0700

    fix(assets/transformers): remove pseudodecorators from generated hooks

--- src/assets/templates/husky/pre-push.mjs
+++ src/assets/templates/husky/pre-push.mjs
@@ -2,11 +2,7 @@
 
 import { readFile } from 'node:fs/promises';
 
-import {
-  // {@symbiote/notInvalid @-xun/run}
-  run
-} from '@-xun/run';
-
+import { run } from '@-xun/run';
 import { createGenericLogger } from 'rejoinder';
 
 const log = createGenericLogger({ namespace: 'husky-hook:pre-push' });
