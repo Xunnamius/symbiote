4622d77b8dbe6c643a89cc16b7f2f59cc5d27edf
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2026-05-02 22:02:37 -0500

    refactor: renovate assets

--- babel.config.cjs
+++ babel.config.cjs
@@ -14,13 +14,14 @@ const { deepMergeConfig } = require('./node_modules/@-xun/symbiote/dist/src/asse
 
 const debug = createDebugLogger({ namespace: 'symbiote:config:babel' });
 
-module.exports = deepMergeConfig(
+const config = deepMergeConfig(
   moduleExport({
     derivedAliases: getBabelAliases(),
     ...assertEnvironment({ projectRoot: __dirname })
   }),
   {
     // Any custom configs here will be deep merged with moduleExport's result
+    //
     // You may wish to enable explicit exports references for improved testing
     // DX, but be aware that it is currently a wee buggy as of 5/2025 (fix it!)
     //
@@ -32,6 +33,7 @@ module.exports = deepMergeConfig(
   }
 );
 
+module.exports = config;
 debug('exported config: %O', module.exports);
 
 function getBabelAliases() {
